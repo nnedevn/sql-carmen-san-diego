3ae618df41c5af32e09e8e08d6542dcd5ce68da5
Nick_edu <nick_edu@nameless.local>
2017-12-19 10:38:13 -0800

    Solved

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,19 +1,33 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+select name, population from country where region = 'Southern Europe' order by population asc limit 5;
 
+-- Holy See (Vatican City State) |       1000
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+select code from country where name ='Holy See (Vatican City State)';
+--VAT
+select * from countrylanguage where countrycode ='VAT';
+
+--VAT         | Italian  | t          |          0
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+select countrycode from countrylanguage where language = 'Italian' and percentage = 100;
+
+--SMR
+
+select name from country where code = 'SMR';
+
+--San Marino
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
@@ -21,11 +35,28 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+select * from city where countrycode = 'SMR';
+
+-- 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+--  3171 | San Marino | SMR         | San Marino        |       2294
+
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+
+select * from city where name like 'Serra%';
+
+-- 265 | Serra      | BRA         | Esp�rito Santo    |     302666
+--  3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+
+select country from country where code = 'BRA';
+
+--(BRA,Brazil,"South America","South America",8.5474e+06,1822,170115000,62.9,776739.00,804108.00,Brasil,"Federal Republic","Fernando Henrique Cardoso",211,BR)
+-- (1 row)
+
+
 
 
 
@@ -33,14 +64,29 @@
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+select capital from country where name ='Brazil';
+
+--211
+
+select name from city where id = 211;
+
+--Bras�lia
+
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
 
+
+
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
+
+
+
+
+
 
 -- Our play date of late has been unusually fun –
 -- As an agent, I'll say, you've been a joy to outrun.
@@ -50,8 +96,12 @@
 -- In a city of ninety-one thousand and now, eighty five.
 
 
+select name from city where population = 91084;
+
+4060 | Santa Monica | USA         | California |      91084
+
+
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
-
--- She's in ____________________________!
+-- She's in ________Santa Monica______!
